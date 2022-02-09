f5c57855854cfbfb24ad0b9dc0943f711c1f7545
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-02-09 17:49:13 +0200

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import {reset} from "redux-form";
 export const createContact = formValues => async dispatch => {
 
     const response = await axios.post('http://localhost:3001/contacts', formValues)
