0ac2c7f7ca1a9a95c4b6d46b8b145c78f78a39d9
uraalexkuch <57583435+uraalexkuch@users.noreply.github.com>
2022-02-09 22:42:55 +0200

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const createContact = formValues => async dispatch => {
 
-    const response = await axios.post('http://localhost:3000/api/add', formValues)
+    const response = await axios.post('https://calm-veiled-queen.glitch.me/api/add', formValues)
 
     //Add Contact to Redux Store
     dispatch({type: 'ADD_CONTACT', payload:response.data});
